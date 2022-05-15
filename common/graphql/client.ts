cbcbe9a87a69df8fde75cba6d70b3b1437a9601d
kriziu <brunodzi07@gmail.com>
2022-05-15 19:35:22 +0200

    Update client.ts

--- common/graphql/client.ts
+++ common/graphql/client.ts
@@ -13,7 +13,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      Authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
