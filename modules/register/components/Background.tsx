c47f9a388cea7d315e5bd22128850c66d732ad65
kriziu <brunodzi07@gmail.com>
2022-05-25 21:37:55 +0200

    Update Background.tsx

--- modules/register/components/Background.tsx
+++ modules/register/components/Background.tsx
@@ -8,6 +8,7 @@ const Background = () => (
       loop
       className="pointer-events-none absolute top-0 left-0 -z-20 h-screen w-screen object-cover"
       poster="/img/videoPoster.png"
+      playsInline
     />
   </>
 );
